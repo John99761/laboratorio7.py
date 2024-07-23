42055dfe760daff86f01d35a6c90d65400be62ee
John99761 <113964308+John99761@users.noreply.github.com>
2024-07-22 20:33:06 -0700

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,19 +1,35 @@
-import cv2 
+import cv2
+import os
 
-face_cascade = cv2.CascadeClassifier("haarcascade-frontalface_default.xml")
+
+cascPath = os.path.join(cv2.data.haarcascades, "haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier(cascPath)
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    
     _, img = cap.read()
+
+    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+    
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+    
     cv2.imshow("img", img)
+
+    # Romper el bucle si se presiona la tecla 'Esc'
     k = cv2.waitKey(30)
-    if k == 27:  # Press 'ESC' to exit
+    if k == 27:
         break
+
+
+cap.release()
+cv2.destroyAllWindows()
 
